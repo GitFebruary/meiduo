4db9c29752eedb7f02af61990affe7d096345943
xwp <xwp_fullstack@163.com>
2018-12-01 17:20:11 +0800

    删除无用类与函数

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -13,10 +13,6 @@ from django.conf import settings
 from users.utils import merge_cart_cookie_to_redis
 
 
-def meiduo():
-
-    pass
-
 class OauthLoginViewQQ(APIView):
     """
         构造qq登录的跳转链接
